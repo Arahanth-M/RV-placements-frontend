10e468149225accab34b756fb8e57fcfb91abf35
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-09-15 11:22:56 +0530

    updating mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,49 @@
 // export default App;
 
 
+// import { Provider } from "react-redux";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import appStore from "./utils/appStore"; 
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Home from "./components/Home";
+// import Login from "./components/Login";
+// import CompanyStats from "./components/CompanyStats";
+// import CompanyDetails from "./components/CompanyDetails";
+// import Contact from "./components/Contact";
+// import Chatbot from "./components/Chatbot";
+// import InternshipExperience from "./components/InternshipExperience";
+
+// function App() {
+//   return (
+//     <Provider store={appStore}>
+//       <BrowserRouter basename="/">
+//         <div className="flex flex-col min-h-screen">
+//           {/* Fixed header */}
+//           <Header />
+
+//           {/* Add top padding equal to header height */}
+//           <main className="flex-grow pt-16 md:pt-20">
+//             <Routes>
+//               <Route path="/" element={<Home />} />
+//               <Route path="/login" element={<Login />} />
+//               <Route path="/companystats" element={<CompanyStats />} />
+//               <Route path="/companies/:id" element={<CompanyDetails />} /> 
+//               <Route path="/internshipExperience" element={<InternshipExperience />} />
+//               <Route path="/contact" element={<Contact />} />
+//               <Route path="/chatbot" element={<Chatbot />} />
+//             </Routes>
+//           </main>
+
+//           <Footer />
+//         </div>
+//       </BrowserRouter>
+//     </Provider>
+//   );
+// }
+
+// export default App;
+
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import appStore from "./utils/appStore"; 
@@ -58,11 +101,11 @@ function App() {
     <Provider store={appStore}>
       <BrowserRouter basename="/">
         <div className="flex flex-col min-h-screen">
-          {/* Fixed header */}
+          {/* Fixed Header */}
           <Header />
 
-          {/* Add top padding equal to header height */}
-          <main className="flex-grow pt-16 md:pt-20">
+          {/* Main content with padding so it never hides under header */}
+          <main className="flex-grow pt-20 md:pt-24">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
@@ -82,3 +125,4 @@ function App() {
 }
 
 export default App;
+
