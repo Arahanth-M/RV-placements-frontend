9fc8957c65e5482db9c975d222a3cb9f0ec7d4b1
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-09-21 17:02:12 +0530

    updating oauth2 routes backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,6 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7779" : "/api/";
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7779" : "/api/";
 
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:7779"
+    : "http://lastminuteplacementprep.in/api";
