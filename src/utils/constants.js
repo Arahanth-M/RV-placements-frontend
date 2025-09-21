2579c021eacf6c2a080cc3e6f69a5fd651894732
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-09-21 16:38:30 +0530

    upadting the routes of oauth

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,2 @@
-// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7779" : "/api/";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7779" : "/api/";
 
-export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:5000/api" // <-- backend dev URL
-    : "http://lastminuteplacementprep.in/api"; // <-- production backend
