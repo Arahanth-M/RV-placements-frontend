dff655e2aec59b09f0a073a95e219169e4d414ce
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-09-21 15:57:38 +0530

    updating routes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7779" : "/api/";
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7779" : "/api/";
+
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:5000/api" // <-- backend dev URL
+    : "http://lastminuteplacementprep.in/api"; // <-- production backend
