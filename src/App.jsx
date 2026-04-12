8979db274a42c2c3e88dbd7fc58bd5e9c7764c1b
akshatha-anil07 <akshathaa.cs22@rvce.edu.in>
2026-04-12 19:06:34 +0530

    included header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { useLayoutEffect } from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import appStore from "./utils/appStore";
 import { AuthProvider } from "./utils/AuthContext";
 import { PremiumProvider } from "./utils/PremiumContext";
 import { ThemeProvider } from "./utils/ThemeContext";
-import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import AuthCallback from "./components/AuthCallback";
@@ -27,18 +28,27 @@ import PlacementPopupWrapper from "./components/PlacementPopupWrapper";
 import Leaderboard from "./components/Leaderboard";
 import AIInterviews from "./components/AIInterviews";
 
+/** Reset window scroll on client-side navigation (e.g. home marquee → company details). */
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
   return (
     <Provider store={appStore}>
       <BrowserRouter basename="/">
+        <ScrollToTop />
         <AuthProvider>
           <PremiumProvider>
             <ThemeProvider>
               <div className="flex flex-col min-h-screen bg-theme-app text-theme-primary">
-                <Sidebar />
+                <Header />
 
-              <main className="flex-grow">
+              <main className="flex-grow pt-6 sm:pt-8 pb-2 px-2 sm:px-4 md:px-6">
                 <Routes>
                   <Route path="/" element={<Home />} />
                   <Route path="/auth/callback" element={<AuthCallback />} />
